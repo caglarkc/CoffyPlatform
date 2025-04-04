46191c80ba519fa0f64a621c39869c88999019ac
u210709019 <alicaglarkocer@gmail.com>
2025-04-04 20:59:15 +0300

    .

--- backend/services/admin-service/src/config/database.js
+++ backend/services/admin-service/src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
   // MongoDB connection settings
   mongodb: {
     url: 'mongodb://localhost:27017',
-    dbName: 'adminAuthServiceDB',
+    dbName: 'adminServiceDB',
     options: {
       serverSelectionTimeoutMS: 5000, // 5 saniye sonra timeout
       socketTimeoutMS: 45000, // 45 saniye sonra socket timeout
@@ -15,7 +15,7 @@ module.exports = {
   },
   testDB: {
     url: 'mongodb://localhost:27017',
-    dbName: 'adminAuthServiceDBTest',
+    dbName: 'adminServiceDBTest',
     options: {
       serverSelectionTimeoutMS: 5000, // 5 saniye sonra timeout
     }
